7ae96eea66c53c6c9d341fa7496c83cd8e90cc7a
estaesta <ahmadforesta@gmail.com>
2023-12-25 17:50:39 +0700

    feat: initial ui

--- main.go
+++ main.go
@@ -3,25 +3,26 @@ package main
 import (
 	"net/http"
 
-	"github.com/a-h/templ"
-	// "github.com/labstack/echo/v4"
-	// "github.com/labstack/echo/v4/middleware"
+	"github.com/estaesta/ytarchive-web/utils"
 	"github.com/estaesta/ytarchive-web/view"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	// e := echo.New()
+	e := echo.New()
 
-	// e.Use(middleware.Logger())
+	e.Use(middleware.Logger())
+	e.Static("/static", "assets")
 
 	component := view.Index()
 
-	// e.GET("/", func(c echo.Context) error {
-	// 	return c.String(http.StatusOK, "Hello, World!")
-	// })
-	// e.Logger.Fatal(e.Start(":1323"))
+	e.GET("/", func(c echo.Context) error {
+		return utils.Render(c, http.StatusOK, component)
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 
-	http.Handle("/", templ.Handler(component))
-
-	http.ListenAndServe(":1323", nil)
+	// http.Handle("/", templ.Handler(component))
+	//
+	// http.ListenAndServe(":1323", nil)
 }
